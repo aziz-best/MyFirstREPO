545fa6d794d65694c42bcd5470e5526c382c40a0
Azizbek Gaybullaev <00004937@wiut.uz>
2018-01-09 16:50:24 +0500

    modified last version

--- JAVAscript.js
+++ JAVAscript.js
@@ -5,3 +5,31 @@ function sayHello(){
 function sayGoodbye(){
 	console.log("GoodBye");
 }
+
+//1
+var  a = 3;
+console.log('Perimeter is ', 4*a);
+console.log('Formula is P=4*a');
+//2
+var a;
+console.log('S =', a*a);
+
+//3
+var  a=3, b=4;
+console.log('S =', a*b);
+console.log('P =', 2*(a+b));
+
+//4
+var  d=3, pi=3.14;
+console.log('L =', pi*d);
+
+//5
+var  a=3;
+console.log('V =',a*a*a);
+console.log('S =',6*a);
+
+//6
+var  a=3, b=7;
+console.log('M =',(a+b)/2);
+
+//7
